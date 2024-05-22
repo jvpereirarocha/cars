2476c59fa081da635e110e2b578a8870f5abe261
Joao Victor <jvpr.tech@gmail.com>
2024-05-21 23:28:58 -0300

    setting admin

--- cars/admin.py
+++ cars/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from cars.models import Brand, Vehicle
+
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+    list_filter = ("name",)
+
+
+class VehicleAdmin(admin.ModelAdmin):
+    list_display = ("vehicle_model", "brand", "year", "fuel_type" ,"license_plate")
+    search_fields = ("vehicle_model", "brand", "fuel_type")
+    list_filter = ("brand", "fuel_type", "number_of_doors", "transmission")
+
+
+admin.site.register(Brand, BrandAdmin)
+admin.site.register(Vehicle, VehicleAdmin)
