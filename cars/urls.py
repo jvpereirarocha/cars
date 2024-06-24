7107397155f9b1431695a6b9f5f146c235a141e9
Joao Victor <jvpr.tech@gmail.com>
2024-06-23 23:56:21 -0300

    add new urls from new views

--- cars/urls.py
+++ cars/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from cars.views import VehicleListView
+from cars.views import RegisterView, VehicleListView, LoginView
 
+
+app_name = 'cars'
 
 urlpatterns = [
-    path("vehicles/", VehicleListView.as_view())
+    path("register/", RegisterView.as_view(), name='register'),
+    path("login/", LoginView.as_view(), name='login'),
+    path("vehicles/", VehicleListView.as_view(), name='vehicles_list'),
 ]
